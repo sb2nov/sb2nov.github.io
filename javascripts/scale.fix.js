b9f4ece9d3ad30e8662f2ed1df074df39cbeef6e
Sourabh <sb2nov@gmail.com>
2013-05-18 01:10:55 +0530

    jquery for single sidebar

--- javascripts/scale.fix.js
+++ javascripts/scale.fix.js
@@ -1,14 +1,3 @@
-function createSidebar(){
-    document.getElementById('sidebar').innerHTML = '<h1><a href=\"index.html\">Sourabh Bajaj</a></h1><p>Software Developer</p><hr width=\"50%\">\
-        <p class=\"view\"><a href=\"about.html\">About Me<small>Who I am</small></a></p>\
-        <p class=\"view\"><a href=\"experience.html\">Experience<small>Where I have worked</small></a></p>\
-        <p class=\"view\"><a href=\"projects.html\">Projects<small>What I have done</small></a></p><p></p>\
-        <hr width=\"50%\">\
-        <h3>Contact</h3>\
-        <p class=\"view\"><a href=\"mailto:sourabhbajaj@gatech.edu\">Email<small>Email me</small></a></p>\
-        <p class=\"view\"><a href=\"https://github.com/sb2nov\">Github<small>Code lives here</small></a></p>\
-        <p class=\"view\"><a href=\"http://www.linkedin.com/in/sbajaj9\">LinkedIn<small>Connect with me</small></a></p>';
-}
 var metas = document.getElementsByTagName('meta');
 var i;
 if (navigator.userAgent.match(/iPhone/i)) {
@@ -26,5 +15,3 @@ function gestureStart() {
     }
   }
 }
-
-window.onload = createSidebar;
